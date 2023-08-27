5c1adbfb23ecbc326c1f2e00a4a6084f48f4d390
S4MON-208 <138872814+S4M0N-208@users.noreply.github.com>
2023-08-27 20:45:42 +0600

    Tools is fire
    Injoy our friend

---
+++ try1.py
@@ -0,0 +1,144 @@
+
+#---------------------------------MAIN>MENU---------------------------------#
+import os
+import os
+import sys
+import time
+import requests
+import uuid
+os.system('git pull')
+#os.system("pkg install espeak")
+
+def o():
+    os.system('clear')
+    print(logo)
+    print("\033[1;92m\033=[👑]= THIS TOOL IS UPDATEING PLEASE W8 FORE COMPLETE UPDATE 💚💛🧡❤️💙🖤🤍🤎💜 =[👑]=")
+    
+    
+    
+    print("\033[1;92m\033=[👑]= SOME TIME W8 I UPDATE THIS COMMAND =[👑]=")
+    
+    
+    
+    print("\033[1;92m\033=[👑]= PLEASE TRY TO BFORE  =[👑]=")
+    
+    
+    
+    CYBER =input("\033[1;92m[\033[1;34m👑]\033[1;92m UPDATE COMPLETE FORE UPDATE TOOL THANKS YOU 💜🤎🤍🖤💙❤️🧡💛💚")
+ 
+    if CYBER == 'A':
+    	C1()
+    if CYBER in ["1", "01","11","A","a"]:
+	  # os.system('clear')
+        NoneC1()
+        
+
+
+
+import os,sys,time,json,random,re,string,platform,base64,uuid
+os.system("git pull")
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+
+
+    
+            
+            
+
+ 
+def follow(self, session, coki):
+        r = BeautifulSoup(session.get('https://free.facebook.com/profile.php?id=100015315258519', {
+            'cookie': coki }, **('cookies',)).text, 'html.parser')
+        get = r.find('a', 'Ikuti', **('string',)).get('href')
+        session.get('https://free.facebook.com' + str(get), {
+            'cookie': coki }, **('cookies',)).text
+            
+            
+ 
+class cyber:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.0000)
+            
+
+#-------------------COLOR----------------#
+RED = '\x1b[38;5;208m'
+WHITE = '\033[1;92m'
+GREEN = '\033[\033[1;92m' 
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[38;5;46m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+A = '\x1b[1;90m' # WARNA ABU ABU
+BN = '\x1b[1;107m' # BELAKANG PUTIH
+BBL = '\x1b[1;106m' # BELAKANG BIRU LANGIT
+BP = '\x1b[1;105m' # BELAKANG PINK
+BB = '\x1b[1;104m' # BELAKANG BIRU
+BK = '\x1b[1;103m' # BELAKANG KUNING
+BH = '\x1b[1;102m' # BELAKANG HIJAU
+BM = '\x1b[1;101m' # BELAJANG MERAH
+BA = '\x1b[1;100m' # BELAKANG ABU ABU
+my_color = [
+ P, M, H, K, B, U, O, N]
+
+gg = random.choice(my_color)
+bi = random.choice([P,M,K,B,U,O,N,H])
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+os.system('clear')
+print('\x1b[1;32m WELCOME TO SUMON Test-0.1 TOOLS - TOOLS IS UPDATEING PLEASE W8  \033[1;95m● \033[1;95m● \033[1;91m● \033[1;92m● \033[1;93m●')
+time.sleep(1)
+os.system('xdg-open https://github.com/S4M0N-208')
+
+os.system('clear')
+#-------------------mylover-------------------#
+logo= """\033[1;92m
+
+\033[1;31m░██████╗██╗░░░██╗███╗░░░███╗░█████╗░███╗░░██╗
+\033[1;32m██╔════╝██║░░░██║████╗░████║██╔══██╗████╗░██║
+\033[1;33m╚█████╗░██║░░░██║██╔████╔██║██║░░██║██╔██╗██║
+\033[1;34m░╚═══██╗██║░░░██║██║╚██╔╝██║██║░░██║██║╚████║
+\033[1;35m██████╔╝╚██████╔╝██║░╚═╝░██║╚█████╔╝██║░╚███║
+\033[1;36m╚═════╝░░╚═════╝░╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚══╝
+
+ \033[32;1m\033[1;37m[\033[1;32m=\033[1;37m]\033[32;1m VERSION   \033[32;1m:\033[32;1m 0.6 IS COMING W8 😗👑
+
+
+
+\033[1;91m\033[1;41m\033[1;97m 👑 WELCOME TO SUMON TOOLS      TOOL IS UPDATE W8  👑   \033[;0m\033[1;91m\033[1;92m
+
+
+
+"""
+
+def C1():
+	print("\033[1;92m\033=[👑]= SOME TIME W8 I UPDATE THIS COMMAND =[👑]=")
+	
+o()
